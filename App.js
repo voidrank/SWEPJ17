ec068ede02397f0ab7135c4473a00ea7e59b8a37
voidrank <voidrank@gmail.com>
2017-10-23 12:38:54 +0800

    color

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     this.state = {
       loggedIn: false,
       loadedCookie: false,
-      username: "username",
+      username: "",
       password: ""
     }
   }
@@ -20,31 +20,30 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>
-          LOGIN
-        </Text>
-        <Text style={styles.text}>
-          username
-        </Text>
-        <TextInput
-          style={styles.username}
-          onChangeText={(username, props)=>this.setState({username})}
-          value={this.state.username}
-        />
-        <Text style={styles.text}>
-          password
-        </Text>
-        <TextInput
-          style={styles.password}
-          onChangeText={(password, props)=>this.setState({password})}
-          secureTextEntry={true}
-          value={this.state.password}
-        />
-        <Button
-          onPress={this.onSubmit}
-          title="SUBMIT"
-          style={styles.button}
-        />
+        <View style={styles.form}>
+          <TextInput
+            style={styles.username}
+            onChangeText={(username, props)=>this.setState({username})}
+            value={this.state.username}
+            placeholder={"username"}
+          />
+          <TextInput
+            style={styles.password}
+            onChangeText={(password, props)=>this.setState({password})}
+            secureTextEntry={true}
+            value={this.state.password}
+            placeholder={"password"}
+          />
+          <Button
+            onPress={this.onSubmit}
+            title="SUBMIT"
+            color="#70bd56"
+            theme='dark'
+            raised={true}
+            overrides={{backgroundColor: "#70bd56"}}
+            style={styles.button}
+          />
+        </View>
       </View>
     );
   }
@@ -53,7 +52,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#79c363',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -61,7 +60,29 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 17
   },
-  username: { width: 80, height: 18, borderWidth: 1, borderColor: 'gray' },
-  password: { width: 80, height: 18, borderWidth: 1, borderColor: 'gray' },
-  button: {}
+  form: {
+    backgroundColor: '#ffffff',
+    width: 240,
+    height: 100,
+    flex: 0.3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.5,
+    shadowColor: '#000000',
+  },
+  username: { 
+    width: 180, height: 40, 
+    backgroundColor: "#eeeeee",
+    padding: 6, margin: 5
+  },
+  password: { 
+    width: 180, height: 40, 
+    backgroundColor: "#eeeeee",
+    padding: 6, margin: 5
+  },
+  button: {
+    width: 180, height: 40,
+    backgroundColor: "#70bd56"
+  }
 });
